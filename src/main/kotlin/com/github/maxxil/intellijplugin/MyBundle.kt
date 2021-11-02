60dd6cde8c9f8fb011893f47bc46dca4ee5b293c
GitHub Action <action@github.com>
2021-11-02 15:01:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/maxxil/intellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/maxxil/intellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.maxxil.intellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/maxxil/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/maxxil/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.maxxil.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maxxil.intellijplugin.MyBundle
 
 class MyApplicationService {
 
